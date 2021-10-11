fa83ad333b0fa4097c2c38d47ece524baa1ad2af
Student <a@a.com>
2021-10-11 12:45:41 +0200

    Tabliczka mnozenia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
         String lancuch="Witaj", imie="Maks";
 	    System.out.println(String.format("%s posiadaczu imienia: %s",lancuch, imie));
+        for(int i=1; i<11; i++){
+            for(int j=1; j<11; j++){
+                System.out.print(String.format("%4d", i*j));
+            }
+            System.out.println();
+        }
         System.exit(0);
     }
 }
